3a4bddae569762a9b239aa377294b8363389f734
Phil Rzewski <phil@brimsecurity.com>
2021-10-08 15:49:20 -0700

    Remove shell script files. They're in README now.

--- edu/prep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd "$(dirname "$0")"
-
-sqlite3 -json sqlite/cdeschools.sqlite "select * from schools;" > json/schools.json
-sqlite3 -json sqlite/cdeschools.sqlite "select * from satscores;" > json/satscores.json
-zq -z -i json -I shape-schools.zed '| sort School' json/schools.json > zson/schools.zson
-zq -z -i json -I shape-satscores.zed '| sort sname' json/satscores.json > zson/satscores.zson
